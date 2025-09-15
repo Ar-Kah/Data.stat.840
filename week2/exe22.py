53ff4c59fc836f3a21eaa016e399410c27776f72
Ar-Kah <aaro.karhu19@gmail.com>
2025-09-15 12:19:05 +0300

    word lematization

--- week2/exe22.py
+++ week2/exe22.py
@@ -1,8 +1,24 @@
+import numpy as np
 import nltk
 from pathlib import Path
 import re
 import bs4
 import requests
+
+
+
+def tagtowordnet(postag):
+    wordnettag=-1
+    if postag[0]=='N':
+        wordnettag='n'
+    elif postag[0]=='V':
+        wordnettag='v'
+    elif postag[0]=='J':
+        wordnettag='a'
+    elif postag[0]=='R':
+        wordnettag='r'
+    return(wordnettag)
+
 
 def getbooktext(parsedpage):
     book_split = str(parsedpage).split("\n")
@@ -84,31 +100,65 @@ def getbookurls(parsedpage, N=10):
 
 
 def cleanuptext(text):
-    stripped_text = text.strip()
-    stripped_text = stripped_text.strip("[]_ *")
-    stripped_text = " ".join(stripped_text.split())
-    return stripped_text
+    split_text = text.split()
+    clean_text_array = []
+    for word in split_text:
+        stripped_text = word.strip("[]_ *")
+        stripped_text = stripped_text.lower()
+        clean_text_array.append(stripped_text)
+
+    clean_text = " ".join(clean_text_array)
+    return clean_text
+
+def stemtext(nltktexttostem):
+    stemmer = nltk.stem.porter.PorterStemmer()
+    stemmedtext=[]
+    for text in nltktexttostem:
+        # Stem the word
+        stemmedword=stemmer.stem(text)
+        # Store the stemmed word
+        stemmedtext.append(stemmedword)
+    return(stemmedtext)
+
+def lematizetext(nltktexttolemmatize):
+    lemmatizer=nltk.stem.WordNetLemmatizer()
+    # Tag the text with POS tags
+    taggedtext=nltk.pos_tag(nltktexttolemmatize)
+    # Lemmatize each word text
+    lemmatizedtext=[]
+    for l in range(len(taggedtext)):
+        # Lemmatize a word using the WordNet converted POS tag
+        wordtolemmatize=taggedtext[l][0]
+        wordnettag=tagtowordnet(taggedtext[l][1])
+        if wordnettag!=-1:
+            lemmatizedword=lemmatizer.lemmatize(wordtolemmatize,wordnettag)
+        else:
+            lemmatizedword=wordtolemmatize
+        # Store the lemmatized word
+        lemmatizedtext.append(lemmatizedword)
+    return(lemmatizedtext)
 
 def wordprssessing():
 
     directory = Path("/home/omakone3/Programming/Data.stat.840/week2/books/")
 
-    nltk.download('punkt')
-    sentenceSplitter=nltk.data.load('tokenizers/punkt/english.pickle')
 
+    crawled_nltktexts = []
     for number, txt_file in enumerate(directory.glob("*.txt")):
-        if number == 2:
+        if number == 1:
             break
         # Open .txt file to start word prossessing
         print(f"Opening file: {txt_file.name}")
         with open(txt_file, 'r') as file:
             text = file.read()
             clean_text = cleanuptext(text)
-            jees = sentenceSplitter(clean_text)
-            for x, jou in enumerate(jees):
-                if x == 1000:
-                    break
-                print(jou)
+            tokenized_words = nltk.word_tokenize(clean_text)
+            nltk_text = nltk.Text(tokenized_words)
+            crawled_nltktexts.append(nltk_text)
+
+            file.close()
+
+    return crawled_nltktexts
 
 def main():
     """
@@ -121,6 +171,8 @@ def main():
     @omakone3@archlinux
     """
 
+    nltk.download('wordnet')
+    nltk.download('punkt')
     # save the url of Project Gutenbergs top ebooks
     # webpage_url = 'https://www.gutenberg.org/browse/scores/top'
     # webpage_html = requests.get(webpage_url)
@@ -137,8 +189,54 @@ def main():
     #     # Save the book contents into txt files
     #     getbooktext(bookpage_parsed)
 
-    # Handle word prossesing
-    wordprssessing()
+    # Here whe already have saved the top N books into local memory to save some RAM.
+    # By calling the word prossessing we go through the book contents and prosses the
+    # text so that we can do stemming and lemitization later. The returned array will
+    # be in nltk Text format.
+    nltk.download('averaged_perceptron_tagger_eng')
+    nltktexts = wordprssessing()
+
+    crawled_lematizedtexts = []
+    for nltktext in nltktexts:
+        lematizedtext = lematizetext(nltktext)
+        lematizedtext = nltk.Text(lematizedtext)
+        crawled_lematizedtexts.append(lematizedtext)
+
+    vocabularies = []
+    indicies_in_vocabularies = []
+
+    for lematized in crawled_lematizedtexts:
+        uniqueresults = np.unique(lematized, return_inverse=True)
+        uniquewords = uniqueresults[0]
+        wordindices=uniqueresults[1]
+        vocabularies.append(uniquewords)
+        indicies_in_vocabularies.append(wordindices)
+
+    # Unify vocabulary into big vocab
+    tempvocabulary=[]
+    for k in range(len(crawled_lematizedtexts)):
+        tempvocabulary.extend(vocabularies[k])
+        # Find the unique elements among all vocabularies
+    uniqueresults=np.unique(tempvocabulary, return_inverse=True)
+    del(tempvocabulary)
+    unifiedvocabulary=uniqueresults[0]
+    wordindices=uniqueresults[1]
+
+    # Translate previous indices to the unified vocabulary.
+    # Must keep track where each vocabulary started in
+    # the concatenated one.
+    vocabularystart=0
+    indices_in_unifiedvocabulary=[]
+    for k in range(len(crawled_lematizedtexts)):
+        # In order to shift word indices, we must temporarily
+        # change their data type to a Numpy array
+        tempindices=np.array(indicies_in_vocabularies[k])
+        tempindices=tempindices+vocabularystart
+        tempindices=wordindices[tempindices]
+        indices_in_unifiedvocabulary.append(tempindices)
+        vocabularystart=vocabularystart+len(vocabularies[k])
+
+    print(unifiedvocabulary[1000:1020])
 
 if __name__ == '__main__':
     main()
