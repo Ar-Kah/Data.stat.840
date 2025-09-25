9604c8fb4e90615e277070dc0f4aacb3b12ae37a
Ar-Kah <aaro.karhu19@gmail.com>
2025-09-25 11:33:30 +0300

    exercise 3.3 (c) done with added comments

--- week3/exe33.py
+++ week3/exe33.py
@@ -9,7 +9,8 @@ import re
 # import functions that are used a lot
 from exe32 import getbookcontents, getbooktext, cleanuptext, lematizetext, findwordindex
 
-def compute_p_values(n_vocab, distanceoccurrences, absdistancemeans, absdistancevariances, overallabsdistancemean, overallabsdistancevariance, overalldistancecount, absdistancepvalues ):
+def compute_p_values(n_vocab, distanceoccurrences, absdistancemeans, absdistancevariances,
+                     overallabsdistancemean, overallabsdistancevariance, overalldistancecount, absdistancepvalues ):
     for m in range(n_vocab):
         # Find pairs of word m
         tempindices=np.nonzero(distanceoccurrences[m,:]>1)[1]
@@ -49,7 +50,7 @@ def print_top_p_collections(wordstring, distanceoccurrences, absdistancepvalues,
     lowest_meandistances_indices=np.argsort(np.squeeze(np.array(absdistancepvalues[mywordindex,tempindices].todense())),axis=0)
     # Print the top-50 lowest-distance pairs
     print('\nLowest p-values\n')
-    for k in range(min(50,len(lowest_meandistances_indices))):
+    for k in range(min(20,len(lowest_meandistances_indices))):
         otherwordindex=tempindices[lowest_meandistances_indices[k]]
         # Print the words, their absolute distances (mean+std) and distances (mean+std)
         print('{!s}--{!s}: {:d} occurrences, absdist: {:.1f} +- {:.1f}, offset: {:.1f} +- {:.1f}, pvalue: {:f}'.format(
@@ -131,27 +132,41 @@ def count_word_pair_occurances(textinindicies, latestoccurencepositions, distanc
     # End of count_occurrances
 
 
-def print_N_lowestmeandistance(N, absdistancemeans, distanceoccurrences, words):
+def print_N_lowestmeandistance(N, absdistancemeans, absdistancepvalues, distanceoccurrences, words):
 
-    all_pairs = []
+    min_occurrences = 5
+
+    all_pairs_nn_an = []
+    words_tagged = nltk.pos_tag(words)
 
     for wordindex, word in enumerate(words):
-        tempindices = np.nonzero(distanceoccurrences[wordindex,:] > 1)[1]
-        for j in tempindices:
-            if j <= wordindex:   # skip duplicates
+        tempindices = np.nonzero(distanceoccurrences[wordindex,:] >= min_occurrences)[1]
+        for otherwordindex in tempindices:
+
+            tag_of_word = words_tagged[wordindex][1][0]
+            tag_of_other_word = words_tagged[otherwordindex][1][0]
+
+            # Skip over wordparis that are not noun-noun or noun-adjective pairs
+            if not ((tag_of_word in ['N', 'J']) and (tag_of_other_word in ['N', 'J'])):
                 continue
-            mean_dist = absdistancemeans[wordindex, j]
+
+            if otherwordindex <= wordindex:   # skip duplicates
+                continue
+            
+            mean_dist = absdistancemeans[wordindex, otherwordindex]
             # convert sparse to scalar safely
-            all_pairs.append((wordindex, j, mean_dist))
+            p_value = absdistancepvalues[wordindex, otherwordindex]
+            all_pairs_nn_an.append((wordindex, otherwordindex, mean_dist, p_value))
 
     # Sort by mean distance ascending
-    all_pairs_sorted = sorted(all_pairs, key=lambda x: x[2])
+    all_pairs_sorted = sorted(all_pairs_nn_an, key=lambda x: x[2])
 
     # Take top N
     top_N_pairs = all_pairs_sorted[:N]
 
-    for word_i, word_j, mean_d in top_N_pairs:
-        print(words[word_i], words[word_j], mean_d)
+    print(f"Print the {N} word pairs with the lowest absmeandistance that occured atleast {min_occurrences} times in the text")
+    for word_i, word_j, mean_d, p in top_N_pairs:
+        print(f"{words[word_i]}, {words[word_j]}, absmeandistance: {mean_d:.2f}, p value: {p:f}")
 
     # End of print_N_lowestmeandistance
     # -----------------------------------
@@ -300,12 +315,13 @@ def main():
         absdistancemeans[mywordindex,tempindices].todense())),axis=0)
     N = 20
     # Print n lowest meandistance indices
-    print_N_lowestmeandistance(N, absdistancemeans, distanceoccurrences, remainingvocabulary)
 
     absdistancepvalues = scipy.sparse.lil_matrix((vocab_size, vocab_size))
 
     compute_p_values(vocab_size, distanceoccurrences, absdistancemeans, absdistancevariances, overallabsdistancemean,
                         overallabsdistancevariance, overalldistancecount, absdistancepvalues)
+
+    print_N_lowestmeandistance(N, absdistancemeans, absdistancepvalues, distanceoccurrences, remainingvocabulary)
 
     print_top_p_collections('dog', distanceoccurrences, absdistancepvalues, remainingvocabulary,
                             absdistancemeans, absdistancevariances, distancemeans,
@@ -318,3 +334,37 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+Comments on exercise 3.3 output
+
+Print the 20 word pairs with the lowest absmeandistance that occured atleast 5 times in the text
+air, mid, absmeandistance: 1.00, p value: 0.000000
+brother, wild, absmeandistance: 1.00, p value: 0.000000
+club, fang, absmeandistance: 1.00, p value: 0.000000
+half-breed, scotch, absmeandistance: 1.00, p value: 0.000000
+red, sweater, absmeandistance: 1.00, p value: 0.000000
+beast, primordial, absmeandistance: 1.20, p value: 0.000014
+dominant, primordial, absmeandistance: 1.20, p value: 0.000014
+neck, shoulder, absmeandistance: 1.20, p value: 0.000014
+foot, stagger, absmeandistance: 1.40, p value: 0.000266
+live, thing, absmeandistance: 1.50, p value: 0.000001
+club, law, absmeandistance: 1.57, p value: 0.000249
+toil, trace, absmeandistance: 1.57, p value: 0.000051
+circle, silent, absmeandistance: 1.60, p value: 0.000323
+dog, want, absmeandistance: 1.60, p value: 0.000047
+hind, leg, absmeandistance: 1.60, p value: 0.001510
+dog, outside, absmeandistance: 1.62, p value: 0.000240
+eat, kill, absmeandistance: 1.67, p value: 0.000302
+john, thornton, absmeandistance: 1.75, p value: 0.000000
+beast, dominant, absmeandistance: 1.80, p value: 0.000026
+dog, southland, absmeandistance: 1.80, p value: 0.001655
+
+The book contains a lot of atmosferic and describing elements
+and the book gives of a primitive vibe. For example the use of
+adjectives such as wild, dominant and primordial coupled with words
+like kill, beast and dog. In any case this fits well with the
+title of the book "Call of the wild". You would expect somthing
+like this in a book called like this
+"""
